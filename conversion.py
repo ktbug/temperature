e15ae285578f80030fad94d989623a244423bbf3
Katie Kosak <katie.kosak@gmail.com>
2018-09-05 11:38:46 +0100

    Add documentary

--- conversion.py
+++ conversion.py
@@ -1,10 +1,14 @@
 def kelvin_to_celsius(temp):
+    """Convert a temperature"""
+    
 	return temp - 273.15
 
 def celsius_to_fahr(temp):
+    """" Another one"""
 	return temp*(9/5) +32
 
 def kelvin_to_fahr(temp):
+    """"Comment Again """
 	temp_c = kelvin_to_celsius(temp)
 	result = celsius_to_fahr(temp_c)
 	return result
